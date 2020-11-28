137cf0ef4e00c8eeb58df87b39fd593fab61e967
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-29 03:05:55 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan62-LivingThing/src/pbo2/pkg10116417/latihan62/livingthing/LivingThing.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan62.livingthing;
+
+/**
+ *
+ *
+ */
+public abstract class LivingThing {
+    
+    public void walk(String Nama){
+        
+    }
+    
+    public void breath(String nama){
+        System.out.println(nama + " Bernafas ");
+    }
+    
+    public void eat(String nama){
+        System.out.println(nama + " Makan ");
+    }
+}
---
+++ PBO2-10116417-Latihan62-LivingThing/src/pbo2/pkg10116417/latihan62/livingthing/PBO210116417Latihan62LivingThing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan62.livingthing;
+
+/**
+ *
+ * @author 
+ *  NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM MELAKUKAN KEGIATAN
+ */
+public class PBO210116417Latihan62LivingThing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Human hm = new Human();
+        hm.setNama("Gilang Panji Wanenng Pati");
+        hm.walk(hm.getNama());
+        hm.breath(hm.getNama());
+        hm.eat(hm.getNama());
+       
+    
+    }
+    
+}
